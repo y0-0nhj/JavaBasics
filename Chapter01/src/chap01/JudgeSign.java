d89ddf99691347a1bc2581408dc4a801a9cb8df9
yun00 <yun00@DESKTOP-F5N6OD1>
2021-05-20 17:25:28 +0900

    Second Commit...

--- Chapter01/src/chap01/JudgeSign.java
+++ Chapter01/src/chap01/JudgeSign.java
@@ -1,5 +1,19 @@
 package chap01;
 
-public class JudgeSign {
+import java.util.Scanner;
 
+public class JudgeSign {
+	// 입력한 정숫값이 양수인지 음수인지 0인지 판단합니다.
+
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("정수를 입력하시오.");
+		int n = sc.nextInt();
+		
+		if(n>0) System.out.println("입력한 정수는 양수입니다.");
+		else if(n<0) System.out.println("입력한 정수는 음수입니다.");
+		else System.out.println("입력한 정수는 0입니다.");
+		
+	}
 }
