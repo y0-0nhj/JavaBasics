ad7c254a6b5a87585a3c037417123084daf2e49c
윤혜지 <yun003318@naver.com>
2021-05-28 19:32:37 +0900

    선형 검색

--- Chapter03/src/chap03/SeqSearch.java
+++ Chapter03/src/chap03/SeqSearch.java
@@ -17,6 +17,25 @@ public class SeqSearch {
 		}
 	}
 	
+	// Q3 풀이
+	static int searchIdx(int[] a, int n, int key, int[] idx) {
+		int cnt=0;
+		for(int i=0; i<n; i++) {
+			if(a[i]==key) {
+				idx[cnt] = i;
+				cnt++;
+			}
+		}
+		
+		System.out.print("idx[] = ");
+		System.out.print("{"+idx[0]);
+		for(int i=1; i<cnt; i++) 
+			System.out.print(", "+idx[i]);
+		System.out.print("}");
+
+		return cnt;
+		}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
@@ -38,5 +57,23 @@ public class SeqSearch {
 			System.out.println("그 값의 요소가 없습니다.");
 		else
 			System.out.println(key+"은(는) x["+idx+"]에 있습니다.");
+		System.out.println();
+		
+		// Q3. 요솟수가 n인 배열 a에서 key와 일치하는 모든 요소의 인덱스를 배열 idx의 맨 앞부터 순서대로 저장하고,
+		// 일치한 요솟수를 반환하는 메서드를 작성하라.
+		// 예를 들어 요솟수가 8인 배열 a의 요소가 {1, 9, 2, 9, 4, 6, 7, 9}이고
+		// key가 9면 배열 idx에 {1, 3, 7}을 저장하고 3을 반환합니다.
+		int[] idx2 = new int[num];
+		System.out.println("\n배열 idx의 개수 : "+searchIdx(x, num, key, idx2));
+	
+		
+		// Q4. 
+
 	}
+	
+
+	
+	
+	
+	
 }
