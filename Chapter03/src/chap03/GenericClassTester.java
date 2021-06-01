22f8a78d45f2fe48558bc6f09cc977e08243554b
윤혜지 <yun003318@naver.com>
2021-06-01 16:20:54 +0900

    Generic 제네릭 예제

---
+++ Chapter03/src/chap03/GenericClassTester.java
@@ -0,0 +1,25 @@
+package chap03;
+
+// 제네릭 클래스의  예
+public class GenericClassTester {
+
+	// 제네릭 클래스의 파라미터를 T라고 작성한다.
+	static class GenericClass<T> {
+		private T xyz;
+		GenericClass(T t) {	// 생성자
+			this.xyz = t;
+		}
+		T getXyz() {
+			return xyz;
+		}
+	}
+	
+	public static void main(String []args) {
+		// 다음과 같이 파라미터에 String을 넘길 수도 있고 Integer를 넘길 수도 있습니다.
+		GenericClass<String> s = new GenericClass<String>("ABC");
+		GenericClass<Integer> n = new GenericClass<Integer>(15);
+		
+		System.out.println(s.getXyz());
+		System.out.println(n.getXyz());
+	}
+}
