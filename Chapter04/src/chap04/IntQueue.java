614410d203b4973d264d6c9d84960b1e2eb5b241
윤혜지 <yun003318@naver.com>
2021-06-04 00:09:49 +0900

    Queue 큐 - 링버퍼로 큐 생성

---
+++ Chapter04/src/chap04/IntQueue.java
@@ -0,0 +1,12 @@
+package chap04;
+
+// int형 큐
+public class IntQueue {
+	private int max; 	//큐의 용량
+	private int front;	// 첫번째 요소 커서
+	private int rear;	// 마지막 요소 커서
+	private int num;	// 현재 데이터 수
+	private int[] que;	// 큐 본체
+	
+	// 실행 시 예외 : 
+}
