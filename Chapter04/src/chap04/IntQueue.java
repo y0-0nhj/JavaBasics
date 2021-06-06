caa3ccca064f97a6cb5a048b4e6cb0607ebf8c3f
윤혜지 <yun003318@naver.com>
2021-06-06 22:28:02 +0900

    Queue 큐 - 링버퍼로 큐 생성

--- Chapter04/src/chap04/IntQueue.java
+++ Chapter04/src/chap04/IntQueue.java
@@ -28,4 +28,15 @@ public class IntQueue {
 			max = 0;
 		}
 	}
+	
+	// 큐에 데이터를 인큐
+	public int enque(int x) throws OverflowIntQueueException {
+		if(num>=max)
+			throw new OverflowIntQueueException();	// 큐가 가득 참.
+		que[rear++] = x;
+		num++;
+		if(rear==max)
+			rear = 0;
+		return x;
+	}
 }
