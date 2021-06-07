a8dad38c2de95a6d2742c560c3836c07ff4e4173
윤혜지 <yun003318@naver.com>
2021-06-07 18:43:55 +0900

    Queue 큐 - 링버퍼로 큐 생성

--- Chapter04/src/chap04/IntQueue.java
+++ Chapter04/src/chap04/IntQueue.java
@@ -39,4 +39,69 @@ public class IntQueue {
 			rear = 0;
 		return x;
 	}
+	
+	// 큐에 데이터를 디큐
+	public int deque() throws EmptyIntQueueException {
+		if(num<=0)
+			throw new EmptyIntQueueException();		// 큐가 비어있음.
+		int x = que[front++];
+		num--;
+		if(front==max)
+			front = 0;
+		return x;
+	}
+	
+	// 큐에서 데이터를 피크(프런트 데이터를 들여다 봄)
+	public int peek() throws EmptyIntQueueException {
+		if(num <= 0)
+			throw new EmptyIntQueueException();		// 큐가 비어있음.
+		return que[front];
+	}
+	
+	// 큐에서 x를 검색하여 인덱스(찾지 못하면 -1)를 반환
+	public int indexOf(int x) {
+		for(int i=0; i<num; i++) {
+			int idx = (i+front) % max;
+			if(que[idx]==x)				// 검색 성공
+				return idx;
+		}
+		return -1;						// 검색 실패
+	}
+	
+	// 큐를 비움
+	public void clear() {
+		num = front = rear = 0;
+	}
+	
+	// 큐의 용량을 반환
+	public int capacity() {
+		return max;
+	}
+
+	
+	// 큐에 쌓여 있는 데이터 수를 반환
+	public int size() {
+		return num;
+	}
+	
+	// 큐가 비어있는가?
+	public boolean isEmpty() {
+		return num<=0;
+	}
+	
+	// 큐가 가득찼는가?
+	public boolean isFull() {
+		return num>=max;
+	}
+	
+	// 큐 안의 모든 데이터를 프런트 -> 리어 순으로 출력
+	public void dump() {
+		if(num<=0)
+			System.out.println("큐가 비어있습니다.");
+		else {
+			for(int i=0; i<num; i++)
+				System.out.print(que[(i+front) % max] + " ");
+			System.out.println();
+		}
+	}
 }
