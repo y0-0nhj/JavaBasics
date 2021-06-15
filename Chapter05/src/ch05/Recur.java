bc13c711afa970c2164dc0653633db2832567bb4
윤혜지 <yun003318@naver.com>
2021-06-15 17:32:47 +0900

    재귀 알고리즘 분석 - 하향식 분석 recur 메서드

---
+++ Chapter05/src/ch05/Recur.java
@@ -0,0 +1,24 @@
+package ch05;
+
+import java.util.Scanner;
+
+// 재귀 함수 이해하기
+public class Recur {
+	// 재귀함수
+	static void recur(int n) {
+		if(n>0) {
+			recur(n-1);
+			System.out.println(n);
+			recur(n-2);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("정수  : ");
+		int x = sc.nextInt();
+		
+		recur(x);
+	}
+}
